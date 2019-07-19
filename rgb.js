c6e4c231d617d5b2d0e06139467a0f2af712f228
Bhavya Kapoor <bhavyarocks19@gmail.com>
2019-07-19 16:00:07 +0530

    added colours logic

--- rgb.js
+++ rgb.js
@@ -9,21 +9,50 @@ $(function() {
     "rgb(255, 0, 255)"
     ];
 
+    let correctcolour = "";
+    let alreadywon = false;
+
     const setcolours = function()
     {
         let n = $(".square").length;
         for (let i = 0; i < n; i++) 
         {
-            $(`.square:nth-of-type(${i + 1})`).css("background-color", colours[i]);
+            let asquare = $(`.square:nth-of-type(${i + 1})`);
+            asquare.css("background-color", colours[i]);
+            asquare.click(()=>
+            {
+                let pickedcolour = colours[i];
+                let message = $("#message");
+                let thissquare = $(`.square:nth-of-type(${i + 1})`);
+                if(pickedcolour === correctcolour)
+                {
+                    colourall();
+                    message.text("Correct!");
+                    alreadywon = true;
+                }
+                else
+                {
+                    if(alreadywon === false)
+                    {
+                        thissquare.css("background-color","#232323");
+                        message.text("Try Again");
+                    }
+                }
+            });
         }
     }
 
     const setcolourname = function()
     {
-        let givencolour = colours[3];
-        $("#color").html("<h2>" + givencolour + "</h2>");
+        correctcolour = colours[3];
+        $("#color").html("<h2>" + correctcolour + "</h2>");
     }
 
-    setcolours();
+    const colourall = function()
+    {
+        $(".square").css("background-color",correctcolour);
+    }
+
     setcolourname();
+    setcolours();
 });
